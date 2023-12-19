ead08581e5043ef6b0ad916cda0fb5f8ffac70ba
Vlad <vladekshibarov@yandex.ru>
2023-12-19 20:49:17 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.function.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(a, b);//Ошибка в результате деления на ноль.
+        calc.println.accept(c);
+    }
+}
+
+class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : 0; // исправил тут!!!
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
+
+
